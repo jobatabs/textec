daad24af96467d9cda95915be718d142bc1a4a90
mlenni <lenni.martikainen@gmail.com>
2024-11-25 17:00:41 +0200

    add double quotes to all attributes in tests

--- src/story_tests/vars.py
+++ src/story_tests/vars.py
@@ -1,4 +1,4 @@
 COGNITIVE_APPRENTICESHIP_BIBTEX = \
     '@Article{Cog1991,\n  author   = "Allan Collins and John Seely Brown and Ann Holum",\n ' + \
     ' title    = "Cognitive apprenticeship: making thinking visible",\n  ' + \
-    'journal  = "American Educator",\n  year     = 1991\n}'
+    'journal  = "American Educator",\n  year     = "1991"\n}'
